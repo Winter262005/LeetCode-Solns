92d043f458663e990cdc497df296452fd3cf8e17
Shubham Chaudhari <151992419+Winter262005@users.noreply.github.com>
2025-10-02 15:14:59 +0530

    Create threeSum.java

---
+++ threeSum.java
@@ -0,0 +1,44 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length < 3){
+          return new ArrayList<>();
+        }
+        
+        Set<List<Integer>> result = new HashSet<>();
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length - 2; i++){
+          int j = i+1;
+          int k =nums.length - 1;
+          
+          while(j<k){
+            int sum = nums[i] + nums[j] + nums[k];
+            List <Integer> list = new ArrayList<>();
+            if (sum == 0){
+              list.add(nums[i]);
+              list.add(nums[j]);
+              list.add(nums[k]);
+              j++;
+              k--;
+              result.add(list);
+            } else if (sum > 0){
+              k--;
+            } else {
+              j++;
+            }
+          }
+        }
+        return new ArrayList<List<Integer>>(result);
+    }
+    
+    
+    public static void main (String[] args){
+        Solution solution = new Solution();
+        
+        int[] nums = {-1,0,1,2,-1,-4};
+        List<List<Integer>> result = solution.threeSum(nums);
+        
+        System.out.println(result);
+    }
+}
